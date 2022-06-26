457f47ac588b3f4aa5363eeabe76869a0226eaf8
bal120600 <105337596+bal120600@users.noreply.github.com>
2022-06-26 10:01:19 +0800

    Update ManageUser.php

--- LMS5/view/manage_User/ManageUser.php
+++ LMS5/view/manage_User/ManageUser.php
@@ -143,6 +143,50 @@ table, tr, th, td
 
 </div>
 
+  <!-- ======= Contact Section ======= -->
+  <section id="contact" class="section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <h3>Contact Us</h3>
+        </div>
+
+        <div class="row contact-info">
+
+          <div class="col-md-4">
+            <div class="contact-address">
+              <i class="bi bi-geo-alt"></i>
+              <h3>Address</h3>
+              <address>Universiti Malaysia Pahang, <br>26600 Pekan Pahang, <br>MALAYSIA</address>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="contact-phone">
+              <i class="bi bi-phone"></i>
+              <h3>Phone Number</h3>
+              <p><a href="tel:+609-4245600">09-424 5600 (P)</a></p>
+			  <p><a href="tel:+609-5493131">09-549 3131 (G)</a></p>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="contact-email">
+              <i class="bi bi-envelope"></i>
+              <h3>Email</h3>
+              <p><a href="mailto:umplibrary@ump.edu.my">umplibrary@ump.edu.my</a></p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+
+
 <!-- ======= Footer ======= -->
   <footer id="footer">
 
